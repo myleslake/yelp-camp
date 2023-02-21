3bd9c0682af402d41ab05262410560bc1a4339d9
Myles Lake <lakemyles@gmail.com>
2023-02-21 07:34:05 -0600

    add comment for middleware

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
